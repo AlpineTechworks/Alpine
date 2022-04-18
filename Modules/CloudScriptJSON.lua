4e400dd922b6f23b99cb7dd3b73018971ca950dd
kura <89491373+kuraise@users.noreply.github.com>
2022-04-18 13:48:32 -0400

    Create CloudScriptJSON.lua

---
+++ Modules/CloudScriptJSON.lua
@@ -0,0 +1,5 @@
+local CloudScriptJSON = {}
+
+CloudScriptJSON[#CloudScriptJSON + 1] = {Name = "Script Name", Description = "", Script = "https://site.com", Games = [1, 2, 3, 4]}
+
+return CloudScriptJSON
