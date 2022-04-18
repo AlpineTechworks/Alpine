54a3fe8ed163bb5d4b2380f471986ead2e314a84
kura <89491373+kuraise@users.noreply.github.com>
2022-04-18 14:18:58 -0400

    Update Loader.lua

--- Server/Loader.lua
+++ Server/Loader.lua
@@ -1,12 +1,11 @@
+local Notification = loadstring(game:HttpGet('https://raw.githubusercontent.com/ErzaSoftware/ErzaHub/main/Modules/Notification.lua', true))()
 local function load(src)
   loadstring(game:HttpGet(src, true))()
 end
 
 local succ, err = pcall(load,'https://raw.githubusercontent.com/ErzaSoftware/ErzaHub/main/Source/Script.lua');
 if succ then
-  local Notification = loadstring(game:HttpGet('https://raw.githubusercontent.com/ErzaSoftware/ErzaHub/main/Modules/Notification.lua', true))()
   Notification:Send('Erza Succesfully Loaded', 'Erza booted and is ready to use.', 'SConnected')
 elseif not succ then
-  local Notification = loadstring(game:HttpGet('https://raw.githubusercontent.com/ErzaSoftware/ErzaHub/main/Modules/Notification.lua', true))()
-  Notification:Send('Loading Error', err, 'sError')
+  Notification:Send('Error Loading Erza', err, 'sError')
 end
